99d0ce28f67734fa3584301e1579b8ec00833641
qiberry <pspnik84@gmail.com>
2022-06-13 21:57:32 +0300

    Changed localhost to webhost

--- app/src/main/java/com/example/coursework_cli/Settings.java
+++ app/src/main/java/com/example/coursework_cli/Settings.java
@@ -1,7 +1,7 @@
 package com.example.coursework_cli;
 
 public class Settings {
-    public static String postFileEndpoint = "http://192.168.0.162";
+    public static String postFileEndpoint = "http://www.tkachenkon.space";
     public final static int permissionRequestCode = 30;
     public final static int fileSelectRequestCode = 33;
 }
